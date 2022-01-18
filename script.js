a6c35fb445cc653522c576b19122a35eefa0ee0e
Zachary Pick <pickzt18@gmail.com>
2022-01-18 09:15:31 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function getCountryInfo() {
+  let country = document.getElementById("country").value;
+  let arr = fetch("https://restcountries.com/v3.1/name/" + country).then(
+    (data) => data.json().then((arr) => countryData(arr))
+  );
+}
+function countryData(arr) {
+  document.getElementById("info").innerText =
+    "Capital: " +
+    arr[0].capital +
+    "Languages: " +
+    arr[0].languages +
+    "Landlocked: " +
+    arr[0].landlocked +
+    "Area: " +
+    arr[0].area +
+    "Population " +
+    arr[0].population;
+}
+document.getElementById("submit").addEventListener("click", getCountryInfo);
