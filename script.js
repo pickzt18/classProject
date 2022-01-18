8759882eddd693975f8092bfd6ac53acc851eab5
Zachary Pick <pickzt18@gmail.com>
2022-01-18 14:24:33 -0800

    create CovidData function/diseaseRiskLevel

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 function getCountryInfo() {
   let country = document.getElementById("country").value;
+  let countryFood = document.getElementById("country");
+  countryFood = countryFood.options[countryFood.selectedIndex].id;
+  console.log(countryFood);
   let arr = fetch("https://restcountries.com/v3.1/name/" + country)
     .then((data) => data.json())
-    .then((arr) => countryData(arr));
+    .then((arr) => {
+      getCovidData(arr);
+      countryData(arr);
+    });
+}
+function getCovidData(arr) {
+  let covData = fetch(
+    "https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=" +
+      arr[0].altSpellings[0],
+    {
+      headers: { Authorization: "Bearer gQidbFmEFXBfrcUiGekqfGLPyMwA" },
+    }
+  )
+    .then((def) => def.json())
+    .then((covData) => {
+      if (
+        covData.diseaseRiskLevel === "High" ||
+        covData.diseaseRiskLevel === "Extreme"
+      ) {
+        getCountryCuisine();
+      } else {
+        countryData();
+      }
+    });
 }
 function countryData(arr) {
   document.getElementById("results_container").innerText =
@@ -14,14 +40,5 @@ function countryData(arr) {
     arr[0].area +
     " Population " +
     arr[0].population;
-  let a = console.log(
-    fetch(
-      "https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=" +
-        arr[0].altSpellings[0],
-      {
-        headers: { Authorization: "Bearer e6dyy1Z0MeNJRIbUQRwTkuUDR4DWsBAp" },
-      }
-    ).then((def) => def.json())
-  );
 }
 document.getElementById("country").addEventListener("change", getCountryInfo);
