21e88946d62b33bb35c149d48292a07aedc4a3f7
Zachary Pick <pickzt18@gmail.com>
2022-01-18 12:21:21 -0800

    a

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 function getCountryInfo() {
   let country = document.getElementById("country").value;
-  let arr = fetch("https://restcountries.com/v3.1/name/" + country).then(
-    (data) => data.json().then((arr) => countryData(arr))
-  );
+  let arr = fetch("https://restcountries.com/v3.1/name/" + country)
+    .then((data) => data.json())
+    .then((arr) => countryData(arr));
 }
 function countryData(arr) {
-  document.getElementById("info").innerText =
+  document.getElementById("results_title").innerText =
     "Capital: " +
     arr[0].capital +
-    "Languages: " +
-    arr[0].languages +
-    "Landlocked: " +
+    " Landlocked: " +
     arr[0].landlocked +
-    "Area: " +
+    " Area: " +
     arr[0].area +
-    "Population " +
+    " Population " +
     arr[0].population;
+  let a = console.log(
+    fetch(
+      "https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=" +
+        arr[0].altSpellings[0],
+      {
+        headers: { Authorization: "Bearer e6dyy1Z0MeNJRIbUQRwTkuUDR4DWsBAp" },
+      }
+    ).then((def) => def.json())
+  );
 }
-document.getElementById("submit").addEventListener("click", getCountryInfo);
+document.getElementById("country").addEventListener("change", getCountryInfo);
